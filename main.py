f3b677e8802ecd265b7d1d68d9a2c3ee98c829f6
Miriam F Z <miriam.falisova@gmail.com>
2026-01-11 21:29:35 +0100

    Komentáře

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Property:
     def __init__(self, locality: Locality):
         self.locality = locality
 
+# Koeficienty pro různé typy pozemků
 estate_coefficients = {
     "land": 0.85,          # zemědělský pozemek
     "building site": 9.0,  # stavební pozemek
@@ -23,6 +24,7 @@ class Estate(Property):
         self.area = area
 
     def calculate_tax(self):
+        # Výpočet daně pro pozemek: plocha pozemku * koeficient dle typu pozemku (atribut estate_type) * místní koeficient
         tax = ceil(self.area*estate_coefficients[self.estate_type]*self.locality.locality_coefficient)
         return tax
 
@@ -36,10 +38,12 @@ class Residence(Property):
     def calculate_tax(self):
         commercial_index = 1
         if self.commercial:
-            commercial_index *= 2
+            commercial_index *= 2 # Pro podnikání je daň dvojnásobná
+        # Výpočet daně pro pozemek: podlahová plocha * koeficient lokality * 15 (* 2 pokud je daň je dvojnásobná).
         tax = ceil(self.area*self.locality.locality_coefficient*15*commercial_index)
         return tax
 
+# Testovací výpisy
 print (Estate (Locality ("Manětín", 0.8), "land", 900).calculate_tax())
 print (Residence(Locality ("Manětín", 0.8), 120).calculate_tax())
 print (Residence(Locality ("Brno", 3), 90, commercial=True).calculate_tax())
